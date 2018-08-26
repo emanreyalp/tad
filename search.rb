ce26f360889bdc119bfe521f566a40ec5592a425
Gergely Dömök <emanreyalp@users.noreply.github.com>
2018-08-26 12:42:50 +0200

    Rename constans, add correct return to search

--- search.rb
+++ search.rb
@@ -1,15 +1,15 @@
 BMES_TARGYAK = [
-  {code: 'asd', name:  'Analizis 1', descr: 'alma, eper, korte, paradicsom, dinnye, sarga es piros paprika'},
-  {code: 'asd', name: 'Analizis 2', descr: 'salata, hagyma, kaposzta'}
+  {code: 'bmeasd1', name:  'Analizis 1', descr: 'alma, eper, korte, paradicsom, dinnye, sarga es piros paprika'},
+  {code: 'bmeasd2', name: 'Analizis 2', descr: 'salata, hagyma, kaposzta'}
 ]
 
-GDFES_TARGYAK = [
-  {name: 'Analizis', descr: 'alma, korte, sarga paprika, szilva'},
-  {name: 'Szamelmelet', descr: 'hagyma, kaposzta, piros paprika, barack'}
+BEFOGADO_INT_TARGYAI = [
+  {code: 'bgfasd', name: 'Analizis', descr: 'alma, korte, sarga paprika, szilva'},
+  {code: 'bgfqwe', name: 'Szamelmelet', descr: 'hagyma, kaposzta, piros paprika, barack'}
 ]
 
 # Vissza adja a targyak metszetet es kulonbsegeit
-# Ex.: [metszet: ['alma', 'korte'], 
+# Ex.: [metszet: ['alma', 'korte'],
 #       first_min_second: ['eper', 'dinnye'],
 #       second_min_first: ['paprika', 'szilva']]
 #`
@@ -18,18 +18,18 @@ def search(first_class, second_class)
   first_min_second = []
   first_class.split(',').each do |first_word|
     if second_class.include? first_word
-      metszet << first_word 
+      metszet << first_word
     else
       first_min_second << first_word
     end
   end
-  {metszet, first_min_second}
+  {metszet: metszet, first_min_second: first_min_second}
 end
 
 def main
-  
+  {befogado_targy_code: '123', beszamitando_targyak: ['asd']}
   BMES_TARGYAK.each do |bmes_targy|
-    GDFES_TARGYAK.each do |gdfes_targy|
+    BEFOGADO_INT_TARGYAI.each do |gdfes_targy|
       s_res = search(bmes_targy, gdfes_targy)
 
     end
